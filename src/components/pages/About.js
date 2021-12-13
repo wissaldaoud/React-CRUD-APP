5f1293f37cce583da185ff12edd7cb981c13440a
unknown <daoudwissal2000@gmail.com>
2021-12-13 09:28:59 +0100

    my app React

---
+++ src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+         Welcome To our University
+        </p>
+
+        <p className="lead">
+          Parcours technologie de l'informatique
+        </p>
+        <p className="lead"></p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
